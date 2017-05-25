5bc23740988087c5d6a1d65063b702a21ffc4bef
Tanner Lake <tanner.lake@gmail.com>
2017-05-25 11:58:44 -0700

    create Redis as Layer1 service

---
+++ module/main.tf
@@ -0,0 +1,34 @@
+resource "layer0_load_balancer" "redis" {
+  name = "${var.load_balancer_name}"
+  environment = "${var.environment_id}"
+  private = true
+
+  port {
+    host_port = "${var.redis_port}"
+    container_port = "${var.redis_port}"
+    protocol = "tcp"
+  }
+}
+
+resource "layer0_service" "redis" {
+  name = "${var.service_name}"
+  environment = "${var.environment_id}"
+  deploy = "${ var.deploy_id == "" ? layer0_deploy.redis.id : var.deploy_id }"
+  load_balancer = "${layer0_load_balancer.redis.id}"
+  scale = "${var.scale}"
+  wait = true
+}
+
+resource "layer0_deploy" "redis" {
+  name = "${var.deploy_name}"
+  content = "${data.template_file.redis.rendered}"
+}
+
+data "template_file" "redis" {
+  template = "${file("${path.module}/Dockerrun.aws.json")}"
+
+  vars {
+    redis_port = "${var.redis_port}"
+  }
+}
+
---
+++ module/outputs.tf
@@ -0,0 +1,3 @@
+output "load_balancer_url" {
+  value = "${layer0_load_balancer.redis.url}"
+}
